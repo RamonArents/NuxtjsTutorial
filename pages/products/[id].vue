873fc06dfe13f2be8bfe2a535895d3fa846bd8f7
unknown <ramonarents@hotmail.com>
2023-07-08 14:20:52 +0200

    initial commit

---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt
+      necessitatibus laborum veniam illum quos ad fugiat suscipit exercitationem
+      rem beatae!
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style scoped></style>
