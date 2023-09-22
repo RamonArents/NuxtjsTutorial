b7527513dd2746357ed61ebda3f8c8d768e49130
unknown <ramonarents@hotmail.com>
2023-09-22 23:00:25 +0200

    metadata and error pages

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <Head>
+      <Title>Nuxt Dojo | {{ product.title }}</Title>
+      <Meta name="description" :content="product.description" />
+    </Head>
     <ProductDetail :product="product" />
   </div>
 </template>
@@ -10,6 +14,10 @@ const uri = 'https://fakestoreapi.com/products/' + id;
 
 //fetch the product
 const { data: product } = await useFetch(uri, {key:id});
+
+if(!product.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Product not found', fatal:true });
+}
 
 definePageMeta({
     layout:'products'
